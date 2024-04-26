9875c4fb94c8c16b716fc48cf202f4797a97b5df
MatsTauholm <mats.tauholm@lbs.se>
2024-04-26 12:38:16 +0200

    First push

---
+++ Top-down-3D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody rb;
+    public Transform playerTransform;
+    public Camera mainCamera;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+
+    void Update()
+    {
+        Look();
+    }
+
+    void Look()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        Ray ray = mainCamera.ScreenPointToRay(mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 lookAtPoint = hit.point;
+            lookAtPoint.y = playerTransform.position.y; // Keep the same y position as the player
+            playerTransform.LookAt(lookAtPoint);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y) * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(transform.position + movement);
+    }
+}
