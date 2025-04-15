ef0c1f0fd14a1fec45fd0e3d0b35e658a141d255
GitMats85 <mats.tauholm@lbs.se>
2025-04-15 14:58:26 +0200

    Unity 6 upgrade

--- Top-down-3D/Assets/Scripts/CameraController.cs
+++ Top-down-3D/Assets/Scripts/CameraController.cs
@@ -2,42 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using Cinemachine;
+using Unity.Cinemachine;
 using System;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Camera mainCam;
-    [SerializeField] CinemachineVirtualCamera FPCam;
-    [SerializeField] CinemachineVirtualCamera ISOCam;
-    [SerializeField] CinemachineFreeLook TPCam;
+    [SerializeField] CinemachineCamera FPCam;
+    [SerializeField] CinemachineCamera ISOCam;
+    [SerializeField] CinemachineCamera TPCam;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             SwitchCamera(FPCam);
-            mainCam.orthographic = false;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             SwitchCamera(ISOCam);
-            mainCam.orthographic = true;
-            ISOCam.m_Lens.OrthographicSize = 25f;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             SwitchCamera(TPCam);
-            mainCam.orthographic = false;
         }
 
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
-            ISOCam.m_Lens.OrthographicSize += 1; //Change values according to your requirements
+            ISOCam.Lens.OrthographicSize += 1; //Change values according to your requirements
         }
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
-            ISOCam.m_Lens.OrthographicSize -= 1;
+            ISOCam.Lens.OrthographicSize -= 1;
         }
     }
     
